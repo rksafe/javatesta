a18ddbc4a365be087c8afa8083c1678598e8e7c1
rksafe <safehate7@gmail.com>
2022-10-29 15:58:04 -0400

    MyFirst java progam

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class HelloWorld 
+{
+  public static void main(string [] args) 
+   {
+   //prints "Hello , world " to the terminal windows.
+   System.out.println("Hello, world");
+   }
+   }
+   
